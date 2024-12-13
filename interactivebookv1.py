863d08b443aa10a6a7e7e49838229811881073f6
Nathan Rossow <nathanrossow@gmail.com>
2024-12-13 05:28:53 -0600

    Update interactivebookv1.py

--- interactivebookv1.py
+++ interactivebookv1.py
@@ -15,7 +15,7 @@ def main():
         ### Burst Softare Methodology Written By Nathan Rossow - Fast Prototyping With Github, Streamlit Cloud and Pycharm Professional
             
         ### Step 1
-        1. **Ensure you have a GitHub account. If you have a Github.com account, please sign in now.  If not, sign up and create a github account Create a GitHub Account: **
+        1. **Ensure you have a GitHub account. If you have a Github.com account, please sign in now.  If not, sign up and create a github account Create a GitHub Account**
         2. **Login to GitHub: Go to GitHub and log in**
         3. **Create a new repository for your streamlit web application.**
         4. **Next, enter a name for the repository, choose the gitignore for python and choose next.**
